bfd6f9c1fdf5c433712220bfb352f4ba4f036841
Stéphane RAULT <stephane.rault@radicalspam.org>
2016-05-19 16:51:12 +0200

    Add Pagination class

--- widukind_common/flask_utils/queries.py
+++ widukind_common/flask_utils/queries.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import math
 from datetime import datetime
-import uuid
-import time
-import logging
 from pprint import pprint
 
 from flask import current_app, abort, request
+
 from pymongo import ReadPreference
+from pymongo.cursor import Cursor
 
 from widukind_common import constants
 
@@ -22,6 +22,8 @@ __all__ = [
     
     'get_provider',
     'get_dataset',
+    
+    'Pagination'
 ]
 
 def col_providers(db=None):
@@ -46,7 +48,12 @@ def col_counters(db=None):
 
 def complex_queries_series(query={}, 
                            search_attributes=True, 
-                           bypass_args=['limit', 'tags', 'provider', 'dataset']):
+                           bypass_args=['limit', 
+                                        'tags', 
+                                        'provider', 
+                                        'dataset',
+                                        'per_page',
+                                        'page']):
 
     tags = request.args.get('tags', None)
     
@@ -137,3 +144,110 @@ def get_dataset(slug, projection=None):
         
     return dataset_doc
     
+class Pagination(object):
+
+    def __init__(self, iterable, page, per_page):
+
+        if page < 1:
+            abort(404)
+
+        self.iterable = iterable
+        self.page = page
+        self.per_page = per_page
+
+        if isinstance(iterable, Cursor):
+            self.total = iterable.count()
+        else:
+            self.total = len(iterable)
+
+        start_index = (page - 1) * per_page
+        end_index = page * per_page
+
+        self.items = iterable[start_index:end_index]
+        #if isinstance(self.items, Cursor):
+        #    self.items = self.items.select_related()
+        if not self.items and page != 1:
+            abort(404)
+
+    @property
+    def pages(self):
+        """The total number of pages"""
+        return int(math.ceil(self.total / float(self.per_page)))
+
+    def prev(self, error_out=False):
+        """Returns a :class:`Pagination` object for the previous page."""
+        assert self.iterable is not None, ('an object is required '
+                                           'for this method to work')
+        iterable = self.iterable
+        if isinstance(iterable, Cursor):
+            iterable.skip(0)
+            iterable.limit(0)
+        return self.__class__(iterable, self.page - 1, self.per_page)
+
+    @property
+    def prev_num(self):
+        """Number of the previous page."""
+        return self.page - 1
+
+    @property
+    def has_prev(self):
+        """True if a previous page exists"""
+        return self.page > 1
+
+    def next(self, error_out=False):
+        """Returns a :class:`Pagination` object for the next page."""
+        assert self.iterable is not None, ('an object is required '
+                                           'for this method to work')
+        iterable = self.iterable
+        if isinstance(iterable, Cursor):
+            iterable.skip(0)
+            iterable.limit(0)
+        return self.__class__(iterable, self.page + 1, self.per_page)
+
+    @property
+    def has_next(self):
+        """True if a next page exists."""
+        return self.page < self.pages
+
+    @property
+    def next_num(self):
+        """Number of the next page"""
+        return self.page + 1
+
+    def iter_pages(self, left_edge=2, left_current=2,
+                   right_current=5, right_edge=2):
+        """Iterates over the page numbers in the pagination.  The four
+        parameters control the thresholds how many numbers should be produced
+        from the sides.  Skipped page numbers are represented as `None`.
+        This is how you could render such a pagination in the templates:
+
+        .. sourcecode:: html+jinja
+
+            {% macro render_pagination(pagination, endpoint) %}
+              <div class=pagination>
+              {%- for page in pagination.iter_pages() %}
+                {% if page %}
+                  {% if page != pagination.page %}
+                    <a href="{{ url_for(endpoint, page=page) }}">{{ page }}</a>
+                  {% else %}
+                    <strong>{{ page }}</strong>
+                  {% endif %}
+                {% else %}
+                  <span class=ellipsis>…</span>
+                {% endif %}
+              {%- endfor %}
+              </div>
+            {% endmacro %}
+        """
+        last = 0
+        for num in range(1, self.pages + 1):
+            if (num <= left_edge or
+                (num >= self.page - left_current and
+                 num <= self.page + right_current) or
+                num > self.pages - right_edge):
+                if last + 1 != num:
+                    yield None
+                yield num
+                last = num
+        if last != self.pages:
+            yield None
